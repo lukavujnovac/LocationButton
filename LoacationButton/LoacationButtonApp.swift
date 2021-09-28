21d1c804632f82cbaee90964ae62b2d220f2f4c6
Luka <vujnovacluka89@gmail.com>
2021-09-28 19:45:54 +0200

    Initial Commit

---
+++ LoacationButton/LoacationButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoacationButtonApp.swift
+//  LoacationButton
+//
+//  Created by Luka Vujnovac on 28.09.2021..
+//
+
+import SwiftUI
+
+@main
+struct LoacationButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
